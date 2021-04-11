398fecef0b53d1bdefc7341d71233db23a975555
ashermorgan <59518073+ashermorgan@users.noreply.github.com>
2021-04-10 19:09:53 -0700

    Add license and dist file headers

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require("gulp");
+var header = require("gulp-header");
 var minifyCss = require("gulp-clean-css");
 var rename = require("gulp-rename");
 var uglify = require("gulp-uglify");
+var { version } = require("./package.json");
+
+let headerText = "/*\n";
+headerText += ` * data-table${version ? " "+version : ""}\n`;
+headerText += " * (c) 2021 Asher Morgan (MIT)\n";
+headerText += " * https://github.com/ashermorgan/data-table\n";
+headerText += " */\n";
 
 gulp.task("css", function() {
     return gulp.src("./src/data-table.css")
         .pipe(minifyCss())
         .pipe(rename({suffix: ".min"}))
+        .pipe(header(headerText))
         .pipe(gulp.dest("./dist"));
 });
 
@@ -14,6 +23,7 @@ gulp.task("js", function() {
     return gulp.src("./src/data-table.js")
         .pipe(uglify())
         .pipe(rename({suffix: ".min"}))
+        .pipe(header(headerText))
         .pipe(gulp.dest("./dist"));
 });
 
